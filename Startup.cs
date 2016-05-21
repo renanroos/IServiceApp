47060540867a0c141f8ba595b83aa35ca11488ca
Renan Roos <renan-roos@hotmail.com>
2016-05-21 11:35:05 -0300

    Solução
    Solução completa.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IServiceApp.Startup))]
+namespace IServiceApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
